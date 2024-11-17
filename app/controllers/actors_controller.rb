f9bb7f5c7d916bd8f4fcbd3ac64a6cc6771d979c
sahilagr123 <sahilagr@uchicago.edu>
2024-11-17 21:57:08 +0000

    d

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,4 +1,46 @@
 class ActorsController < ApplicationController
+  def update
+    actor_id = params.fetch("the_id")
+    matching_records = Actor.where({ :id => actor_id })
+
+    a = matching_records.at(0)
+
+    a.name = params.fetch("the_name")
+    a.dob = params.fetch("the_dob")
+    a.bio = params.fetch("the_bio")
+    a.image = params.fetch("the_image")
+
+    a.save
+
+    redirect_to("/actors/#{a.id}")
+  end  
+
+
+  def create
+    a = Actor.new
+    a.name = params.fetch("the_name")
+    a.dob = params.fetch("the_dob")
+    a.bio = params.fetch("the_bio")
+    a.image = params.fetch("the_image")
+
+    a.save
+
+    redirect_to("/actors")
+  end
+
+  def destroy
+    the_id = params.fetch("an_id")
+
+    matching_records = Actor.where({ :id => the_id })
+
+    the_actor = matching_records.at(0)
+    
+    the_actor.destroy
+
+    redirect_to("/actors")
+
+  end
+  
   def index
     matching_actors = Actor.all
     @list_of_actors = matching_actors.order({ :created_at => :desc })
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,6 +17,7 @@ class MoviesController < ApplicationController
 
     redirect_to("/movies/#{m.id}")
   end  
+  
   def create
     m = Movie.new
     m.title = params.fetch("the_title")
